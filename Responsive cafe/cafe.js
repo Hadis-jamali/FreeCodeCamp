b4f65e0fa2d2d36b34d8a9fbe399a0018e3c9819
Hadis Jamali <hadisjamali95@gmail.com>
2023-08-18 16:45:19 +0200

    Cafe Responsive V1

---
+++ Responsive cafe/cafe.js
@@ -0,0 +1,5 @@
+function langClicked() {
+  alert(
+    "Hello! I am Hadis Jamali. Passionate Junior Frontend Developer with expertise in HTML, CSS, JavaScript and ongoing learning in React JS. Leveraging a psychology background, I bring a unique perspective to create user-centric interfaces that intuitively cater to user needs, optimize cognitive load, and drive positive user engagement."
+  );
+}
